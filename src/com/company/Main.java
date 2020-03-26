230ea8676ac1abfe206828d28ae31e937462a318
QA engineer <kotarakh@gmail.com>
2020-03-27 01:01:25 +0200

    raw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,19 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	Triangle a = new Triangle(3,5,4);
 	System.out.println(a.getPerimeter());
-	System.out.println(a.getArea());
-	Triangle b = new Triangle(6,10,8);
-	System.out.println(b.getPerimeter());
-	System.out.println(b.getArea());
-	Triangle c = new Triangle(15,9,12);
-	System.out.println(c.getPerimeter());
-	System.out.println(c.getArea());
-	Triangle d = new Triangle(3,5,4);
-	System.out.println(d.getPerimeter());
-	System.out.println(d.getArea());
+//	System.out.println(a.getArea());
+//	Triangle b = new Triangle(6,10,8);
+//	System.out.println(b.getPerimeter());
+//	System.out.println(b.getArea());
+//	Triangle c = new Triangle(15,9,12);
+//	System.out.println(c.getPerimeter());
+//	System.out.println(c.getArea());
+//	Triangle d = new Triangle(3,5,4);
+//	System.out.println(d.getPerimeter());
+//	System.out.println(d.getArea());
 
 
     }
