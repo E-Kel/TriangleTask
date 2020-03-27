59e67c427bdab76241410cc92ce15145f59a12f6
QA engineer <kotarakh@gmail.com>
2020-03-27 04:31:17 +0200

    add arraylist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,16 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Triangle> list;
 
-       // Triangle a = new Triangle(3,5,4);
-        Triangle v = new Triangle(8,4,4); //triangle validation failed
-        //Triangle b = new Triangle(6,10,8);
+        Triangle a = new Triangle(3, 5, 4);
+        Triangle v = new Triangle(8, 4, 4); //triangle validation failed
+        Triangle b = new Triangle(6, 10, 8);
 
-    //	ArrayOfTriangles aa = new ArrayOfTriangles( a,b);
-    	v.getTypes();
-//	System.out.println(a.getArea());
-//	System.out.println(b.getPerimeter());
-//	System.out.println(b.getArea());
-//	Triangle c = new Triangle(15,9,12);
-//	System.out.println(c.getPerimeter());
-//	System.out.println(c.getArea());
-//	Triangle d = new Triangle(3,5,4);
-//	System.out.println(d.getPerimeter());
-//	System.out.println(d.getArea());
+        //	ArrayOfTriangles aa = new ArrayOfTriangles( a,b);
+        v.getTypes();
+        a.getTypes();
+        b.getTypes();
+        //	Triangle c = new Triangle(15,9,12);
+        //	Triangle d = new Triangle(3,5,4);
 
 
     }
